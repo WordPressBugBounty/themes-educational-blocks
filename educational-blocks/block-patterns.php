9f654627df15f1d84263e525ae0baf1c45328977
Sean Murphy <sean@iamseanmurphy.com>
2025-02-27 12:19:16 +0000

    2.6

--- educational-blocks/block-patterns.php
+++ educational-blocks/block-patterns.php
@@ -13,13 +13,13 @@
  * @return void
  */
 function educational_blocks_register_block_patterns() {
-	$block_pattern_categories = array(
+	$educational_blocks_block_pattern_categories = array(
 		'educational-blocks'    => array( 'label' => __( 'Educational Blocks', 'educational-blocks' ) ),
 	);
 
-	$block_pattern_categories = apply_filters( 'educational_blocks_block_pattern_categories', $block_pattern_categories );
+	$educational_blocks_block_pattern_categories = apply_filters( 'educational_blocks_block_pattern_categories', $educational_blocks_block_pattern_categories );
 
-	foreach ( $block_pattern_categories as $name => $properties ) {
+	foreach ( $educational_blocks_block_pattern_categories as $name => $properties ) {
 		if ( ! WP_Block_Pattern_Categories_Registry::get_instance()->is_registered( $name ) ) {
 			register_block_pattern_category( $name, $properties );
 		}
