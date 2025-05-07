4a196301c1d499c54f745d005fb1b8572b4d84eb
Sean Murphy <sean@iamseanmurphy.com>
2025-05-07 12:19:59 +0000

    2.9

--- educational-blocks/functions.php
+++ educational-blocks/functions.php
@@ -38,6 +38,10 @@ function educational_blocks_setup() {
 
 	// Enqueue editor styles.
 	add_editor_style( array( 'assets/css/editor-style.css' ) );
+
+    require get_parent_theme_file_path( '/inc/dashboard/dashboard.php' );
+
+    require get_parent_theme_file_path( '/inc/customizer/customizer.php' );
 }
 endif; // educational_blocks_setup
 add_action( 'after_setup_theme', 'educational_blocks_setup' );
@@ -64,83 +68,27 @@ function educational_blocks_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'educational_blocks_scripts' );
 
-// Get start function
-
 function educational_blocks_enqueue_admin_script($hook) {
-    // Admin JS
-    wp_enqueue_script('educational-blocks-admin-js', get_theme_file_uri('/inc/dashboard/admin.js'), array('jquery'), true);
-    wp_localize_script(
-        'educational-blocks-admin-js',
-        'educational_blocks',
-        array(
-            'admin_ajax'    =>  admin_url('admin-ajax.php'),
-            'wpnonce'           =>  wp_create_nonce('educational_blocks_dismissed_notice_nonce')
-        )
-    );
-    wp_enqueue_script('educational-blocks-admin-js');
-
-    wp_localize_script( 'educational-blocks-admin-js', 'educational_blocks_scripts_localize',
-        array( 'ajax_url' => admin_url( 'admin-ajax.php' ) )
-    );
+    // Enqueue admin JS for notices
+    wp_enqueue_script('educational-blocks-welcome-notice', get_template_directory_uri() . '/inc/dashboard/educational-blocks-welcome-notice.js', array('jquery'), '', true);
+    
+    // Localize script to pass data to JavaScript
+    wp_localize_script('educational-blocks-welcome-notice', 'educational_blocks_localize', array(
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'nonce' => wp_create_nonce('educational_blocks_welcome_nonce'),
+        'dismiss_nonce' => wp_create_nonce('educational_blocks_welcome_nonce'), // Nonce for dismissal
+        'redirect_url' => admin_url('themes.php?page=educational-blocks-guide-page')
+    ));
 }
 add_action('admin_enqueue_scripts', 'educational_blocks_enqueue_admin_script');
 
-//dismiss function 
-add_action( 'wp_ajax_educational_blocks_dismissed_notice_handler', 'educational_blocks_ajax_notice_dismiss_fuction' );
-
-function educational_blocks_ajax_notice_dismiss_fuction() {
-    if (!wp_verify_nonce($_POST['wpnonce'], 'educational_blocks_dismissed_notice_nonce')) {
-        exit;
-    }
-    if ( isset( $_POST['type'] ) ) {
-        $type = sanitize_text_field( wp_unslash( $_POST['type'] ) );
-        update_option( 'dismissed-' . $type, TRUE );
-    }
+function educational_blocks_admin_theme_style() {
+   wp_enqueue_style('educational-blocks-custom-admin-style', esc_url(get_template_directory_uri()) . '/inc/dashboard/dashboard.css');
 }
-
-//get start box
-function educational_blocks_custom_admin_notice() {
-    // Check if the notice is dismissed
-    if ( ! get_option('dismissed-get_started_notice', FALSE ) )  {
-        // Check if not on the theme documentation page
-        $educational_blocks_current_screen = get_current_screen();
-        if ($educational_blocks_current_screen && $educational_blocks_current_screen->id !== 'appearance_page_educational-blocks-guide-page') {
-            $educational_blocks_theme = wp_get_theme();
-            ?>
-            <div class="notice notice-info is-dismissible" data-notice="get_started_notice">
-                <div class="notice-div">
-                    <div>
-                        <p class="theme-name"><?php _e('Educational Blocks', 'educational-blocks'); ?></p>
-                        <p><?php _e('For information and detailed instructions, check out our theme documentation.', 'educational-blocks'); ?></p>
-                    </div>
-                    <div class="notice-buttons-box">
-                        <a class="button-primary livedemo" href="<?php echo esc_url( EDUCATIONAL_BLOCKS_LIVE_DEMO ); ?>" target="_blank"><?php esc_html_e('Live Demo', 'educational-blocks'); ?></a>
-                        <a class="button-primary buynow" href="<?php echo esc_url( EDUCATIONAL_BLOCKS_BUY_PRO ); ?>" target="_blank"><?php esc_html_e('Buy Now', 'educational-blocks'); ?></a>
-                        <a class="button-primary theme-install" href="themes.php?page=educational-blocks-guide-page"><?php _e('Begin Installation', 'educational-blocks'); ?></a> 
-                    </div>
-                </div>
-            </div>
-        <?php
-        }
-    }
-}
-add_action('admin_notices', 'educational_blocks_custom_admin_notice');
-
-//after switch theme
-add_action('after_switch_theme', 'educational_blocks_after_switch_theme');
-function educational_blocks_after_switch_theme () {
-    update_option('dismissed-get_started_notice', FALSE );
-}
-
-//get-start-function-end//
+add_action('admin_enqueue_scripts', 'educational_blocks_admin_theme_style');
 
 // Block Patterns.
 require get_template_directory() . '/block-patterns.php';
-
-require get_parent_theme_file_path( '/inc/dashboard/dashboard.php' );
-
 require get_template_directory() . '/custom-setting.php';
-
-require get_parent_theme_file_path( '/inc/customizer/customizer.php' );
-
 require get_template_directory() .'/inc/TGM/tgm.php';
+require_once get_template_directory() . '/inc/dashboard/welcome-notice.php';
--- educational-blocks/inc/dashboard/dashboard.php
+++ educational-blocks/inc/dashboard/dashboard.php
@@ -5,11 +5,6 @@ function educational_blocks_gettingstarted() {
 	add_theme_page( esc_html__('Begin Installation', 'educational-blocks'), esc_html__('Begin Installation', 'educational-blocks'), 'edit_theme_options', 'educational-blocks-guide-page', 'educational_blocks_guide');
 }
 
-function educational_blocks_admin_theme_style() {
-   wp_enqueue_style('educational-blocks-custom-admin-style', esc_url(get_template_directory_uri()) . '/inc/dashboard/dashboard.css');
-}
-add_action('admin_enqueue_scripts', 'educational_blocks_admin_theme_style');
-
 if ( ! defined( 'EDUCATIONAL_BLOCKS_SUPPORT' ) ) {
 define('EDUCATIONAL_BLOCKS_SUPPORT',__('https://wordpress.org/support/theme/educational-blocks/','educational-blocks'));
 }
@@ -88,7 +83,7 @@ function educational_blocks_guide() {
 						<a class="button-primary buynow" href="<?php echo esc_url( EDUCATIONAL_BLOCKS_BUY_PRO ); ?>" target="_blank"><?php esc_html_e('Buy Now', 'educational-blocks'); ?></a>
 						<a class="button-primary docs" href="<?php echo esc_url( EDUCATIONAL_BLOCKS_PRO_DOC ); ?>" target="_blank"><?php esc_html_e('Documentation', 'educational-blocks'); ?></a>
 					</div>
-						<a class="button-primary bundle-btn" href="<?php echo esc_url( EDUCATIONAL_BLOCKS_BUNDLE_LINK ); ?>" target="_blank"><?php esc_html_e('Wordpress Theme Bundle (120+ Themes at Just $89)', 'educational-blocks'); ?></a>
+						<a class="button-primary bundle-btn" href="<?php echo esc_url( EDUCATIONAL_BLOCKS_BUNDLE_LINK ); ?>" target="_blank"><?php esc_html_e('WordPress Theme Bundle (120+ Themes at Just $89)', 'educational-blocks'); ?></a>
 				</div>
 				<ul style="padding-top:10px">
 					<li class="upsell-educational_blocks"> <div class="dashicons dashicons-yes"></div> <?php esc_html_e('Responsive Design', 'educational-blocks');?> </li>                 
